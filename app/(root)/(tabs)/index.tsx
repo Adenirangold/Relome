7f34174061649fc781b8326169024933e14d221a
adenirangold <adeniranbayogold@gmail.com>
2025-01-07 21:38:19 +0100

    Finished Home page

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,11 +1,13 @@
 import { Card, FeaturedCard } from "@/components/Card";
 import Filters from "@/components/Filters";
+import Noresult from "@/components/Noresult";
 import Search from "@/components/Search";
 import icons from "@/constants/icons";
-import images from "@/constants/images";
+import { getLatestProperties, getProperties } from "@/lib/appwrite";
 import { useGlobalContext } from "@/lib/global-provider";
-import seed from "@/lib/seed";
-import { Link } from "expo-router";
+import { useAppwrite } from "@/lib/useAppwrite";
+import { router, useLocalSearchParams } from "expo-router";
+import { useEffect } from "react";
 import {
   Text,
   SafeAreaView,
@@ -13,22 +15,64 @@ import {
   Image,
   TouchableOpacity,
   FlatList,
-  Button,
+  ActivityIndicator,
 } from "react-native";
-import {} from "react-native-safe-area-context";
 
 export default function Index() {
   const { user } = useGlobalContext();
+  const params = useLocalSearchParams<{ query?: string; filter?: string }>();
+
+  const { data: latestProperties, loading: latestPropertiesLoading } =
+    useAppwrite({
+      fn: getLatestProperties,
+    });
+  const {
+    data: properties,
+    loading: propertiesLoading,
+    refetch,
+  } = useAppwrite({
+    fn: getProperties,
+    params: {
+      query: params.query,
+      filter: params.filter,
+      limit: 6,
+    },
+    skip: true,
+  });
+
+  useEffect(() => {
+    refetch({
+      filter: params.filter,
+      query: params.query,
+      limit: 6,
+    });
+  }, [params.query, params.filter]);
+
+  const handleCardPress = (id: string) => {
+    router.push(`/properties/${id}`);
+  };
   return (
     <SafeAreaView className="h-full bg-white">
       <FlatList
-        data={[1, 2, 3, 4]}
-        keyExtractor={(item) => item.toString()}
+        data={properties}
+        keyExtractor={(item) => item.$id}
         numColumns={2}
         contentContainerClassName="pb-32"
         columnWrapperClassName="flex gap-5 px-5"
         showsVerticalScrollIndicator={false}
-        renderItem={({ item }) => <Card></Card>}
+        ListEmptyComponent={
+          propertiesLoading ? (
+            <ActivityIndicator
+              className="text-primary-300"
+              size="large"
+            ></ActivityIndicator>
+          ) : (
+            <Noresult></Noresult>
+          )
+        }
+        renderItem={({ item }) => (
+          <Card item={item} onPress={() => handleCardPress(item.$id)}></Card>
+        )}
         ListHeaderComponent={
           <View className="px-5">
             <View className=" flex flex-row items-center justify-between mt-5 ">
@@ -62,15 +106,29 @@ export default function Index() {
                   </Text>
                 </TouchableOpacity>
               </View>
-              <FlatList
-                data={[1, 2, 3, 4]}
-                horizontal
-                keyExtractor={(item) => item.toString()}
-                showsHorizontalScrollIndicator={false}
-                bounces={false}
-                contentContainerClassName="flex gap-5 mt-5"
-                renderItem={({ item }) => <FeaturedCard></FeaturedCard>}
-              ></FlatList>
+              {latestPropertiesLoading ? (
+                <ActivityIndicator
+                  className="text-primary-300"
+                  size="large"
+                ></ActivityIndicator>
+              ) : !latestProperties || latestProperties.length === 0 ? (
+                <Noresult></Noresult>
+              ) : (
+                <FlatList
+                  data={latestProperties}
+                  horizontal
+                  keyExtractor={(item) => item.$id}
+                  showsHorizontalScrollIndicator={false}
+                  bounces={false}
+                  contentContainerClassName="flex gap-5 mt-5"
+                  renderItem={({ item }) => (
+                    <FeaturedCard
+                      item={item}
+                      onPress={() => handleCardPress(item.$id)}
+                    ></FeaturedCard>
+                  )}
+                ></FlatList>
+              )}
             </View>
             <View className="flex flex-row items-center justify-between ">
               <Text className="text-xl font-rubik-bold text-black-300">
--- components/Card.tsx
+++ components/Card.tsx
@@ -2,18 +2,23 @@ import { View, Text, TouchableOpacity, Image } from "react-native";
 import React from "react";
 import images from "@/constants/images";
 import icons from "@/constants/icons";
+import { Models } from "react-native-appwrite";
 
 interface props {
   onPress?: () => void;
+  item: Models.Document;
 }
 
-export const FeaturedCard = ({ onPress }: props) => {
+export const FeaturedCard = ({ item, onPress }: props) => {
   return (
     <TouchableOpacity
       onPress={onPress}
       className="h-80 w-60 relative flex flex-col items-start"
     >
-      <Image source={images.japan} className="size-full rounded-2xl"></Image>
+      <Image
+        source={{ uri: item.image }}
+        className="size-full rounded-2xl"
+      ></Image>
       <Image
         source={images.cardGradient}
         className="size-full rounded-2xl absolute bottom-0"
@@ -21,7 +26,7 @@ export const FeaturedCard = ({ onPress }: props) => {
       <View className=" flex flex-row items-center bg-white/90 px-3 py-1.5 rounded-full absolute top-5 right-5">
         <Image className="size-3.5" source={icons.star}></Image>
         <Text className="text-xs font-rubik-bold text-primary-300 ml-1">
-          4.4
+          {item.rating}
         </Text>
       </View>
       <View className=" flex flex-col items-start absolute bottom-5 inset-x-5">
@@ -29,14 +34,12 @@ export const FeaturedCard = ({ onPress }: props) => {
           numberOfLines={1}
           className="text-xl font-rubik-extrabold text-white"
         >
-          Modern Apartment
+          {item.name}
         </Text>
-        <Text className="text-base font-rubik text-white">
-          No 4 Ikorodu, Lagos State
-        </Text>
+        <Text className="text-base font-rubik text-white">{item.address}</Text>
         <View className="flex flex-row items-center w-full justify-between">
           <Text className="text-white text-xl font-rubik-extrabold">
-            $2,250
+            ${item.price}
           </Text>
           <Image className="size-5" source={icons.heart}></Image>
         </View>
@@ -45,7 +48,7 @@ export const FeaturedCard = ({ onPress }: props) => {
   );
 };
 
-export const Card = ({ onPress }: props) => {
+export const Card = ({ item, onPress }: props) => {
   return (
     <TouchableOpacity
       className="flex-1 w-full mt-4 px-3 py-4 rounded-lg bg-white shadow-lg shadow-black-100/70 relative"
@@ -54,23 +57,23 @@ export const Card = ({ onPress }: props) => {
       <View className="flex flex-row items-center absolute px-2 top-5 right-5 bg-white/90 p-1 rounded-full z-50">
         <Image source={icons.star} className="size-2.5" />
         <Text className="text-xs font-rubik-bold text-primary-300 ml-0.5">
-          8.9
+          {item.rating}
         </Text>
       </View>
 
-      <Image source={images.newYork} className="w-full h-40 rounded-lg" />
+      <Image source={{ uri: item.image }} className="w-full h-40 rounded-lg" />
 
       <View className="flex flex-col mt-2">
         <Text className="text-base font-rubik-bold text-black-300">
-          Lucrezia
+          {item.name}
         </Text>
         <Text className="text-xs font-rubik text-black-100">
-          Lagos, Nigeria
+          {item.address}
         </Text>
 
         <View className="flex flex-row items-center justify-between mt-2">
           <Text className="text-base font-rubik-bold text-primary-300">
-            $2,890
+            {item.price}
           </Text>
           <Image
             source={icons.heart}
